828d6403d15cd5b1f24b8f72582e103e1abe5dbb
Alejandro P. Santos III <jayarsantos@gmail.com>
2021-12-16 16:16:40 +0800

    update

--- bin/autoupdate_git.sh
+++ bin/autoupdate_git.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+update_MyConfigs(){
 cd ~/MyConfigs
 
 if [[ -n $(git status -s) ]]; then
@@ -9,6 +10,9 @@ else
     echo "No changes found. Skip pushing."
 fi
 
+}
+
+update_LoanApp(){
 cd ~/LoanApp
 
 if [[ -n $(git status -s) ]]; then
@@ -17,3 +21,19 @@ if [[ -n $(git status -s) ]]; then
 else
     echo "No changes found. Skip pushing."
 fi
+}
+
+case "$1" in
+'DotFiles')
+update_MyConfigs
+;;
+'LoanApp')
+update_LoanApp
+;;
+'All')
+update_LoanApp
+;;
+'')
+echo "script preceeding DotFiles, LoanApp, or All"
+;;
+esac
--- vimwiki/JairelTasks.md
+++ vimwiki/JairelTasks.md
@@ -2,3 +2,14 @@
 - Good day! Please retake your Quarter 2 Quiz #3 in English anak. Thank you!
 - Good day Jairel anak, Please answer again QUIZ # 3 - QUARTER 2 in Science number 1-10 only. Thank you!
 - Good day! Please retake your Quarter 2 Quiz #3, #4 and Activity #2 in English anak. Thank you!
+
+Pending Tasks
+Contents for
+- Values Ed
+- Math
+- MAPEH
+
+Exams With another chance
+- Araling Panlipunan
+- Journalism
+- Science (Quarter 2)
