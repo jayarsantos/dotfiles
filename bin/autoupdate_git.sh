85e43867803d99185809939df703b4ac03eedc1b
Alejandro P. Santos III <jayarsantos@gmail.com>
2021-12-16 13:24:55 +0800

    update

---
+++ bin/autoupdate_git.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+cd ~/MyConfigs
+
+if [[ -n $(git status -s) ]]; then
+    echo "Changes found. Pushing changes..."
+    git add -A && git commit -m 'update' && git push origin main
+else
+    echo "No changes found. Skip pushing."
+fi
+
+cd ~/LoanApp
+
+if [[ -n $(git status -s) ]]; then
+    echo "Changes found. Pushing changes..."
+    git add -A && git commit -m 'update' && git push origin main
+else
+    echo "No changes found. Skip pushing."
+fi
