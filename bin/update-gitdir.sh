a1c4c0af436205bdc0490ed470857e50c6fe98d2
Alejandro P. Santos III <jayarsantos@gmail.com>
2021-12-23 08:44:57 +0800

    update

--- bin/update-gitdir.sh
+++ bin/update-gitdir.sh
@@ -10,6 +10,15 @@ git_push(){
     for dir in ${dirs//:/ }; do
 
         cd $dir
+        
+        VN=$(git describe --abbrev=7 HEAD 2>/dev/null)
+
+        git update-index -q --refresh
+        CHANGED=$(git diff-index --name-only HEAD --)
+        if [ -n "$CHANGED" ]; then
+            VN="$VN-mod"
+            echo "$VN"
+        fi
 
         if [[ 'git status --porcelain' ]]; then
             echo "Changes found. Pushing changes in $dir..."
